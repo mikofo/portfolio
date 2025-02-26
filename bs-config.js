9d7476f7a0a05d0dfadccc1e563d2513dd67085d
mikofo <michaelfoster@humcapital.com>
2025-02-25 16:55:03 -0800

    More progress

---
+++ bs-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  server: {
+    baseDir: "build",
+    index: "index.html",
+  },
+  files: ["build/*"],
+  port: 1234,
+  notify: false,
+  ui: false,
+  ghostMode: false,
+  open: false,
+};
